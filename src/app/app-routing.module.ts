06f79dd7342709199c0954bd6380feba1b0102b1
Barış Yurtçu <baris.yurtcu@lovateknoloji.com>
2023-03-15 22:02:11 +0300

    Auth guard added for other pages than Login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'; 
 import { WelcomeComponent } from './welcome/welcome.component';
-//import { AuthGuard } from "./auth.guard";
+import { AuthGuard } from "./auth.guard";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome', component: WelcomeComponent}
-  //{ path: 'dashboard', component: WelcomeComponent,canActivate: [AuthGuard]}
-  //{ path: '**', component: LoginComponent }
+  { path: 'welcome', component: WelcomeComponent, canActivate: [AuthGuard]},
+  { path: '**', component: WelcomeComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.cookieService.get('currentUser')) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;      
+  }
+  
+}
